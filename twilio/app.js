ad878e971b7084e5984ad526fe7faf70e59e85a6
MIchael LeGrand <michael_legrand@yahoo.com>
2016-10-15 14:41:23 -0400

    I have no idea what I'm doing... trying to integrate heroku posters as the db.

--- twilio/app.js
+++ twilio/app.js
@@ -15,6 +15,7 @@
 // [START app]
 'use strict';
 
+var pg = require('pg');
 var format = require('util').format;
 var express = require('express');
 var bodyParser = require('body-parser').urlencoded({
@@ -73,6 +74,20 @@ app.get('/sms/send', function (req, res, next) {
 // [START receive_sms]
 // [START receive_sms]
 app.post('/sms/receive', bodyParser, function (req, res) {
+
+  pg.defaults.ssl = true;
+  pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+    console.log('Connected to postgres! Getting schemas...');
+
+    client
+      .query('SELECT table_schema,table_name FROM information_schema.tables;')
+      .on('row', function(row) {
+        console.log(JSON.stringify(row));
+      });
+  });
+
+
   var sender = req.body.From;
   var body = req.body.Body;
 
