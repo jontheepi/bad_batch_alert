b9652f33820954934ae908b20a407aee3392072d
MIchael LeGrand <michael_legrand@yahoo.com>
2017-02-27 19:28:18 -0500

    Fixing some logic for ending times on the van command.

--- smsService/userActions.js
+++ smsService/userActions.js
@@ -315,7 +315,7 @@ var UserActions = function()
         vanLocation = 'The van is at Freemont and Riggs Barclay and 23rd until 3:30 PM';
       }
     } else if (n == 3) {
-      if (( h >= 18) && ( h <= 20)){
+      if (( h >= 18) && ( h < 20)){
         vanLocation = 'The van is at Baltimore and Conkling (Highlandtown) until 8:00 PM';
       } else if (( h == 20 && m >= 30) || (h > 20 && h < 22)) {
         vanLocation = 'The van is at Freemont and Laurens until 10:00 PM';
@@ -325,7 +325,7 @@ var UserActions = function()
          vanLocation = 'Van1 is at Pontiac and 9th Ave. Van 2 is at North and Rosedale until 11:30 AM';
       } else if (( h == 12 && m >= 45 ) || ( h == 15 && m <= 30) || (h > 12 && h < 15)) {
          vanLocation = 'Van 1 is at Milton and Monument. Van 2 is at Monroe and Ramsey until 3:30 PM';
-      } else if (h >= 19 && h <= 22 ) {
+      } else if (h >= 19 && h < 22 ) {
          vanLocation = 'The van is at Baltimore and Gay (The Block) until 10:00 PM'; 
       }
     } else if (n == 5){
@@ -333,13 +333,13 @@ var UserActions = function()
         vanLocation = 'Van 1 is at Park Heights and Spaulding. Van 2 is at North and Gay until 11:30 AM';
       } else if (( h == 12 && m >= 45 ) || ( h == 15 && m <= 30) || (h > 12 && h < 15)) {
         vanLocation ='The van is at Fulton and Baker until 3:30 PM';
-      } else if (h >= 18 && h <= 20 ) {
+      } else if (h >= 18 && h < 20 ) {
         vanLocation = 'The van is at Montford and Biddle until 8:00 PM';
       } else if (( h == 20 && m >= 30) || (h > 20 && h < 22)) {
         vanLocation = 'The van is at Monroe and Ramsey until 10:00 PM';
       }
     } else if (n == 6){
-      if (h >= 12 && h <= 16) {
+      if (h >= 12 && h < 16) {
         vanLocation= 'The van is at Fremont and Riggs until 4:00 PM';
       }
     }
