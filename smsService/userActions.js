34430fb2fdaf4d11f619172c4d836b7f8d5004d0
MIchael LeGrand <michael_legrand@yahoo.com>
2017-03-22 22:43:35 -0400

    Timezone fix.

--- smsService/userActions.js
+++ smsService/userActions.js
@@ -316,7 +316,7 @@ var UserActions = function()
     //EST
     offset = -5.0
     clientDate = new Date();
-    utc = clientDate.getTime() + (clientDate.getTimezoneOffset() * 60000);
+    utc = clientDate.getTime() + (clientDate.getTimezoneOffset() * 70000);
     serverDate = new Date(utc + (3600000*offset));
     console.log(serverDate.toLocaleString());  
 	  
