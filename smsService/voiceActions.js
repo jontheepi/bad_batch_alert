56468aab2fd195fbbedb6a45ecc0935b94f864da
MIchael LeGrand <michael_legrand@yahoo.com>
2017-09-11 09:50:21 -0400

    Adding the urls for all the new audio. Hooking up logic to hopefully play it.

--- smsService/voiceActions.js
+++ smsService/voiceActions.js
@@ -19,20 +19,34 @@ var audio = { //all the available messages we can play.
 };
 
 var HELP_STR = "If you would like to know where the Baltimore Needle Exchange Van is right now, Press 2. If you'd like to send an anonymous report to the Bad Batch Alert team, press 3.  If you would like to learn more about the Bad Batch alert service, press 4. If you would like to stop recieving overdose alerts, press 5. If you would like to hear this message again, press 1.";
-var INVALID_LOCATION = "Sorry, this service is only available in the Baltimore metro area. If you'd like to have your area added to the Bad Batch Alert Serivce, send an email to badbatchalert@gmail.com. To hear more options, please press 1 now.";
-var ZIPCODE_CONFIRM = ". If this zipcode is correct press 1 , if not press 2. To hear the zipcode again press three.";
-var REGISTRATION = "Thanks for calling the bad batch alert service, to begin receiving overdose alerts in your area, please enter your 5 digit zipcode now.";
-var REGISTRATION_INVALID = "Sorry, that is not an available option. To begin receiving overdose alerts in your area, please enter the 5 digits of your zip code";
-var REGISTRATION_SUCCESS = "Congratulations! You are all set to receive alerts. If you like to hear more options, press 1 now.";
-var WELCOME = "Thank you for calling Bad Batch Alert, " + HELP_STR;
-var LEAVE = "Thank you for using the Bad Batch Alert service. You are no longer registered to receive alerts. Press 1 for more options.";
-var INFO = "Bad Batch Alert is an anonymous free text message service to help heroin users stay alive in Baltimore City. Find out more at Bad Batch Alert dot com. Press 1 for more options";
-var REPORT = "Please record your message and we will get back to you. If this is a medical emergency, please call nine one one.";
+var INVALID_LOCATION_STR = "Sorry, this service is only available in the Baltimore metro area. If you'd like to have your area added to the Bad Batch Alert Serivce, send an email to badbatchalert@gmail.com. To hear more options, please press 1 now.";
+var ZIPCODE_CONFIRM_STR = ". If this zipcode is correct press 1 , if not press 2. To hear the zipcode again press three.";
+var REGISTRATION_STR = "Thanks for calling the bad batch alert service, to begin receiving overdose alerts in your area, please enter your 5 digit zipcode now.";
+var REGISTRATION_INVALID_STR = "Sorry, that is not an available option. To begin receiving overdose alerts in your area, please enter the 5 digits of your zip code";
+var REGISTRATION_SUCCESS_STR = "Congratulations! You are all set to receive alerts. If you like to hear more options, press 1 now.";
+var WELCOME_STR = "Thank you for calling Bad Batch Alert, " + HELP_STR;
+var LEAVE_STR = "Thank you for using the Bad Batch Alert service. You are no longer registered to receive alerts. Press 1 for more options.";
+var INFO_STR = "Bad Batch Alert is an anonymous free text message service to help heroin users stay alive in Baltimore City. Find out more at Bad Batch Alert dot com. Press 1 for more options";
+var REPORT_STR = "Please record your message and we will get back to you. If this is a medical emergency, please call nine one one.";
+
+var HELP_URL = "help";
+var INVALID_LOCATION_URL = "invalidLocation";
+var ZIPCODE_CONFIRM_URL = "registrationConfirm";
+var REGISTRATION_URL = "registration";
+var REGISTRATION_INVALID_URL = "registrationInvalid";
+var REGISTRATION_SUCCESS_URL = "registrationSuccess";
+var WELCOME_URL = "welcome";
+var LEAVE_URL = "leave";
+var INFO_URL = "info";
+var REPORT_URL = "report";
+
+var AUDIO_SITE = 'www.badbatchalert.com/audio/';
+var EXTENSION = '.wav';
 
 var VoiceActions = function() {
   var self = this;
 
-  function roboGather(response, twiml, message, numDigits)
+  function gather(response, twiml, message, numDigits)
   {
     if (!numDigits) numDigits = 1;
 
@@ -41,7 +55,13 @@ var VoiceActions = function() {
       timeout: 15,
       numDigits: numDigits,
     }, (gatherNode) => {
-      gatherNode.say(message, { voice: 'alice'});
+      if (message.split(' ').length === 1) {//all one word is a file name. a little hax but convenient.
+        var url = AUDIO_SITE + message + EXTENSION;
+        gatherNode.play(message);
+      } else {
+        gatherNode.say(message, { voice: 'alice'});
+      }
+      
     });
 
   };
@@ -85,41 +105,42 @@ var VoiceActions = function() {
         if (zipvalid) {
           var matchedRegionsArray = G.userActions.getRegionsFromZipCode(input);
           if (matchedRegionsArray.length === 0) {
-            roboGather(response, twiml, INVALID_LOCATION);
+            gather(response, twiml, INVALID_LOCATION_URL);
             _activeCall.message = audio.help;
           } else {
             _activeCall.message = audio.registerZip2;
             _activeCall.zip = input;
-            roboGather(response, twiml, input + ZIPCODE_CONFIRM);
+            gatherNode.say(input, { voice: 'alice'});
+            gather(response, twiml, ZIPCODE_CONFIRM_URL);
           }
         } 
       } else {
-        roboGather(response, twiml, REGISTRATION, 5);
+        gather(response, twiml, REGISTRATION_URL, 5);
       }
     } else if((_activeCall.message == audio.registerZip1 || _activeCall.message == audio.registerZip1) && input) {
       //after user has successfully registered a zipcode
       if (input == '1') {
         _activeCall.message = audio.help;
-        roboGather(response, twiml, HELP_STR);
+        gather(response, twiml, HELP_URL);
       }
     } else if (_activeCall.message == audio.registerZip2 && input) {
       // confirm zipcode
       if (input == '1') {
         console.log("registerZip1");
-        roboGather(response, twiml, REGISTRATION_SUCCESS);
+        gather(response, twiml, REGISTRATION_SUCCESS_URL);
         _activeCall.message = audio.help;
         G.userActions.userSetZipCode(G, null, userClient, phone, _activeCall.zip) 
       } else if (input == '2') {
         console.log('registration');
         _activeCall.message = audio.registration;
-        roboGather(response, twiml, REGISTRATION, 5);
+        gather(response, twiml, REGISTRATION_URL, 5);
       } else if (input == '3') {
         console.log('registerZip2');
         _activeCall.message = audio.registerZip2;
-        roboGather(response, twiml, input + ZIPCODE_CONFIRM);
+        gather(response, twiml, input + ZIPCODE_CONFIRM_URL);
       } else {
         console.log('not recognized');
-        roboGather(response, twiml, REGISTRATION_INVALID, 5);
+        gather(response, twiml, REGISTRATION_INVALID_URL, 5);
         _activeCall.message = audio.registration;
       }
     } else if (_activeCall.message == audio.help && input) {
@@ -129,28 +150,28 @@ var VoiceActions = function() {
           var vanLocation =  G.userActions.userVan(G, null, userClient, phone, '');
           var message = vanLocation + ". To Hear more options, press 1 now.";
           _activeCall.message = audio.help;
-          roboGather(response, twiml, message);
+          gather(response, twiml, message);
           break;
         case '3'://send message
-          twiml.say(REPORT, { voice: 'alice'});
+          twiml.play(REPORT_URL);
           twiml.record();
           break;
         case '4'://learn more/info
           //needs to get audio currently on live
-          roboGather(response, twiml, INFO);
+          gather(response, twiml, INFO_URL);
           break;
         case '5'://stop alerts
-          roboGather(response, twiml, LEAVE);
+          gather(response, twiml, LEAVE_URL);
           G.userActions.userLeave(G, null, userClient, phone, '');
           break;
         default:
           _activeCall.message = audio.help;
-          roboGather(response, twiml, HELP_STR);
+          gather(response, twiml, HELP_URL);
           break;
       }
     } else {
       _activeCall.message = audio.help;
-      roboGather(response, twiml, WELCOME);
+      gather(response, twiml, WELCOME_URL);
     } 
 
 
